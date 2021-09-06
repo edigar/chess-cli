3c37e22c95e88f7bbf330b439d8e18afd1da8826
Edigar Herculano <edigar.herculano@gruposbf.com.br>
2021-09-06 11:10:47 -0300

    Feat: Add chessPosition class

---
+++ chess/ChessPosition.cs
@@ -0,0 +1,27 @@
+﻿using chess_cli.board;
+
+namespace chess_cli.chess
+{
+    class ChessPosition
+    {
+        public char column { get; set; }
+        public int line { get; set; }
+
+        public ChessPosition(char column, int line)
+        {
+            this.column = column;
+            this.line = line;
+        }
+
+        public Position toPosition()
+        {
+            return new Position(8 - line, column - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + column + line;
+        }
+
+    }
+}
